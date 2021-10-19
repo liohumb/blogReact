b6ad44f32a853b88ccff6868b0bf2c35cd75e9a8
liohumb <helloliohumb@gmail.com>
2021-10-19 15:20:52 +0200

    contact page

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Contact.css'
 
 export default function Contact() {
     return (
-        <div>
-            
+        <div className="container-contact">
+            <h1>Contactez-nous</h1>
+            <p><i className="uil uil-at"></i> : blogreact@gmail.com</p>
+            <p><i className="uil uil-phone"></i> : 06 06 06 06 06</p>
+            <p>Rejoignez-nous sur les réseaux</p>
+            <ul>
+                <li><i className="uil uil-facebook"></i></li>
+                <li><i className="uil uil-twitter"></i></li>
+                <li><i className="uil uil-instagram"></i></li>
+            </ul>
         </div>
     );
 }
