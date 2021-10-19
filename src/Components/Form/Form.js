f7d4216c355d103be955bd4f7860dde3544748eb
liohumb <helloliohumb@gmail.com>
2021-10-19 14:20:07 +0200

    lier formulaire au state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './Form.css'
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import './Form.css';
 
 export default function Form() {
+    const [article, setArticle] = useState({
+        title: "",
+        body: ""
+    });
+    const dispatch = useDispatch();
+    const handleForm = e => {
+      e.preventDefault();
+    }
+    const handleInputs = e => {
+        if (e.target.classList.contains('inp-title')) {
+            const newObjState = {...article, title: e.target.value};
+            setArticle(newObjState);
+        }
+        else if (e.target.classList.contains('inp-body')) {
+            const newObjState = {...article, body: e.target.value};
+            setArticle(newObjState);
+        }
+    }
+    
     return (
         <>
             <h1 className="title-form">Écrivez un article</h1>
-            <form className="container-form">
+            <form onSubmit={handleForm} className="container-form">
                 <label htmlFor="title">Titre</label>
-                <input type="text" id="title" placeholder="Entrez votre nom"/>
+                <input onChange={handleInputs}
+                       type="text"
+                       id="title"
+                       placeholder="Entrez votre nom"
+                       className="inp-title"
+                />
                 <label htmlFor="article">Votre article</label>
-                <textarea id="article" placeholder="Votre article"/>
+                <textarea onChange={handleInputs}
+                          id="article"
+                          placeholder="Votre article"
+                          className="inp-body"
+                />
                 <button>Envoyer l'article</button>
             </form>
         </>
