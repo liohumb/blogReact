9b7c8dc0c93cdef78d02fa2896df9fe33171fc40
liohumb <helloliohumb@gmail.com>
2021-10-19 13:44:42 +0200

    création du router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Containers/Home/Home";
+import Contact from './Containers/Contact/Contact';
+import AddArticle from './Containers/AddArticle/AddArticle.js';
+import Article from './Containers/Article/Article'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-        <Home/>
-    </div>
+    <>
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/ecrire" exact component={AddArticle}/>
+                <Route path="/contact" exact component={Contact}/>
+                <Route path="/article" exact component={Article}/>
+            </Switch>
+        </Router>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
     const [toggleMenu, setToggleMenu] = useState(false);
@@ -28,9 +29,15 @@ export default function Navbar() {
         <nav>
             {(toggleMenu || largeur > 500) && (
                 <ul className="liste">
-                    <li className="items">Accueil</li>
-                    <li className="items">Écrire</li>
-                    <li className="items">Contact</li>
+                    <Link to="/">
+                        <li className="items">Accueil</li>
+                    </Link>
+                    <Link to="/ecrire">
+                        <li className="items">Écrire</li>
+                    </Link>
+                    <Link to="/contact">
+                        <li className="items">Contact</li>
+                    </Link>
                 </ul>
             )}
             <a onClick={toggleNav} className="btn"><i className="uil uil-list-ul"/></a>
